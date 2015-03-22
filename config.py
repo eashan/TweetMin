998fa8444573072a9d85e763ed18ea238a58ab53
eashan <eashankadam@gmail.com>
2015-03-22 16:32:47 +0530

    tweetmin-2

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ import os
 class BaseConfig(object):
 	DEBUG=False
 	secret_key="my_precious"
-	SQLALCHEMY_DATABASE_URI=os.environ['DATABASE_URI']
+	SQLALCHEMY_DATABASE_URI='postgresql:///tweetmin'
+	os.environ['DATABASE_URI']
 	#for mysql :'mysql://root:eashanrocks@localhost/posts'
 	print SQLALCHEMY_DATABASE_URI
 class DevelopmentConfig(BaseConfig):
