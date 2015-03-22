225682336586eba13a1da942f2ceeae3af9ae931
eashan <eashankadam@gmail.com>
2015-03-22 16:48:03 +0530

    Tweetmin v1.0

--- config.py
+++ config.py
@@ -3,8 +3,9 @@ import os
 class BaseConfig(object):
 	DEBUG=False
 	secret_key="my_precious"
-	SQLALCHEMY_DATABASE_URI='postgresql:///tweetmin'
-	os.environ['DATABASE_URI']
+	SQLALCHEMY_DATABASE_URI=os.environ['DATABASE_URL']
+
+	#(LOCAL MACHINE db)'postgresql:///tweetmin'
 	#for mysql :'mysql://root:eashanrocks@localhost/posts'
 	print SQLALCHEMY_DATABASE_URI
 class DevelopmentConfig(BaseConfig):
